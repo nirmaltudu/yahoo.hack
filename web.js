08aba4a9a2cd786b7d371c5b9fbd61be8f085017
Nirmal Tudu <nirmal_tudu>
2013-07-14 09:20:24 +0530

    execute code web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var q = require('./query_place');
 
 var app = express();
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  q.get_location('panjim', 'kolkata', '18/08/2013');
+  var data = fs.readFileSync('client_data.json');
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
